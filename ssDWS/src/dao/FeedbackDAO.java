d8f0414b5813f4c143389d162c8771915f04e585
amirgc <gcamir15@gmail.com>
2018-11-21 16:39:52 -0500

    new changes on db side

--- ssDWS/src/dao/FeedbackDAO.java
+++ ssDWS/src/dao/FeedbackDAO.java
@@ -1,12 +1,15 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import dal.Dao;
 import model.Feedback;
 
-public class FeedbackDAO {
+public class FeedbackDAO implements Dao{
 	HashMap<String, Feedback> feedbacksDB = new HashMap<>();
 
 	public void addFeedback(Feedback fb) {
@@ -23,6 +26,36 @@ public class FeedbackDAO {
 	
 	public List<Feedback> getAllFeedbacks() {
 		return new ArrayList<Feedback>(feedbacksDB.values());
+	}
+
+	@Override
+	public String getSql() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void unpackResultSet(ResultSet rs) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean InsertUpdate(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<?> Select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object SelectFirstOrDefault() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
